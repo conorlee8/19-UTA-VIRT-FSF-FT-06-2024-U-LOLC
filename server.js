5d8e651a0bd077cee8d9862240d7a33a48649daf
Conor Lee <your emailconorlee8@gmail.com>
2024-09-09 14:06:00 -0500

    changes that should fix it all!

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Root Route to handle GET requests to the homepage
+app.get('/', (req, res) => {
+  res.send('Welcome to the Social Network API!');
+});
+
+// Include your API routes (assuming you have them in a folder called routes)
+const routes = require('./routes'); // Make sure this file/folder exists and is properly configured
+app.use('/api', routes);
+
 // Listen for DB connection before starting the server
 db.once('open', () => {
   app.listen(PORT, () => {
